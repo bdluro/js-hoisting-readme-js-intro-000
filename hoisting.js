45a1276be5d3df9c16f3b0d4cf4cb53849f628fe
Brooke Luro <lurob@me.com>
2020-04-29 20:06:04 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -11,10 +11,9 @@ function callMe() {
 function crazy() {
   // fix the code in here:
   var thisIsCrazy = function (){
-    console.log("hey!!!"); {
-      thisIsCrazy()
-    }
-  }
+    console.log("hey!!!"); 
+}
+    thisIsCrazy();
 }
 
 function sayMyName() {
